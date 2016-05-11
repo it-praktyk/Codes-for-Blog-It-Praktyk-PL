681da23f2b4c5485ca8b6ea9a5f1533219c0e542
Wojciech Sciesinski <wojciech@sciesinski.net>
2016-05-11 20:11:18 +0200

    Codes for 2016-05-11-Mock-Exchange-objects-with-Pester added

---
+++ 201605/2016-05-11-Mock-Exchange-objects-with-Pester/Simple-Exchange-Script.Tests.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module Pester
+
+Import-Module C:\Users\Wojtek\Documents\Scripts\1-MyOnGitHub\Pester-ExchangeServer\Functions\New-MockedExchangeServer.ps1 -Force
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Simple-Exchange-Script" {
+    
+    Context -Name 'Server is CAS' {
+        
+        function Get-ExchangeServer { }
+        
+        Mock -CommandName Get-ExchangeServer -MockWith { New-MockedExchangeServer -IsClientAccessServer $true }
+        
+        It "Server is CAS" {
+            
+            Simple-Exchange-Script | Should Be "The server is Client Access Server"
+            
+        }
+        
+    }
+}
---
+++ 201605/2016-05-11-Mock-Exchange-objects-with-Pester/Simple-Exchange-Script.ps1
@@ -0,0 +1,16 @@
+﻿function Simple-Exchange-Script {
+    
+    $TestExchangeServer = Get-ExchangeServer
+    
+    if ($TestExchangeServer.IsClientAccessServer) {
+        
+        Return "The server is Client Access Server"
+        
+    }
+    else {
+        
+        Return "The server is not Client Access Server"
+        
+    }
+    
+}
